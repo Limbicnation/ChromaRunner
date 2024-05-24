82d321d0112933cd6b6ae63299a7d87f1c2a98c6
gero <gero@limbicnation.com>
2024-05-24 04:23:56 +0200

    Refactor BaseLevel: improve code consistency, readability, and encapsulation

--- Source/SideRunner/BaseLevel.h
+++ Source/SideRunner/BaseLevel.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -11,31 +9,30 @@ class UBoxComponent;
 UCLASS()
 class SIDERUNNER_API ABaseLevel : public AActor
 {
-	GENERATED_BODY()
-	
-public:	
-	// Sets default values for this actor's properties
-	ABaseLevel();
+    GENERATED_BODY()
+    
+public:    
+    // Sets default values for this actor's properties
+    ABaseLevel();
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+public:    
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
+
+    UFUNCTION(BlueprintCallable, Category="My Triggers")
+    UBoxComponent* GetTrigger() const;
+
+    UFUNCTION(BlueprintCallable, Category="My Triggers")
+    UBoxComponent* GetSpawnLocation() const;
 
 protected:
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="My Triggers")
+    UBoxComponent* Trigger;
 
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="My Triggers")
-	UBoxComponent* Trigger;
-
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="My Triggers")
-	UBoxComponent* SpawnLocation;
-
-public:
-
-	UBoxComponent* GetTrigger();
-	UBoxComponent* GetSpawnLocation();
-
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="My Triggers")
+    UBoxComponent* SpawnLocation;
 };
